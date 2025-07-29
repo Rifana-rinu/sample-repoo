8b46625108e286a61e5c23f78de3fd41efa46cec
Rifana-rinu <rifanarahman27@gmail.com>
2025-07-29 19:44:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sample from './Components/Sample'
+
+function App() {
+  return (
+    <div>
+      <Sample/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Components/Sample.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Sample() {
+  return (
+    <div>
+      <h1>Sample good mrng</h1>
+      <p>hellooo every one </p>
+    </div>
+  )
+}
+
+export default Sample
